d961cf5c3423941ad149209b681c9c6528a24c21
yuningyang19 <51691872+yuningyang19@users.noreply.github.com>
2024-05-01 11:10:11 +0800

    new

--- pseudoSVD2.m
+++ pseudoSVD2.m
@@ -7,7 +7,7 @@ function varargout=pseudoSVD2(A)
 % output: varargout{1}: the range matrix H which is orthonormal
 % output: vargargout{2} (optional): flag whether there exists bad part 1: has; 0: doesn't have
 
-
+s = size(A,2);
 AA=Q2cplx(A);
 [Ucc,~,~]=svd(AA,0);    % Ucc keeps the range of Chi_AA, which can be explored
 
@@ -16,9 +16,9 @@ AA=Q2cplx(A);
 % detect the good and bad parts of Ucc by Ucc'*Ucc, ref. our paper
 priH = constrPrimeH(Ucc);   % first construct the H as usual
 pHpH = priH'*priH;          % if Ucc is abnormal, then pHpH will not be orth
-absHH = abs(pHpH);          % compress to real
-nn = vecnorm(absHH);            % compute the magnitude of each column of absHH
-idx = nn>1.0000001;          % findout the abnormal columns of H whose magnitude must be larger than 1
+absHH = abs(pHpH)-eye(s);          % compress to real
+nn = vecnorm(absHH,'Inf');            % compute the magnitude of each column of absHH
+idx = nn>1e-9;          % findout the abnormal columns of H whose magnitude must be larger than 1
 % idx is a logical vector
 % idx means the abnormal col. in H, then back to Ucc, we must duplicate
 % each element of idx.
@@ -38,34 +38,42 @@ Ugq=cplx2Q([Ug,Qc2Qa(Ug)]);
 
 
 Uq = []; has_bad_part = 0;
-if sum(bad_idx_Ucc) > 0.1
-    has_bad_part = 0;
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    % generating Ub
-    % the idea is to use every column of  Ucc(:,badIndex) to generated a
-    % quaternion column, and put them together, termed as Ub; Ub has 2t columns, while its column dim=t;
-    % thus then apply
-    % quaternion svd to Ub to obtain the true range. Of course, before applying
-    % svd, randomly scale every column of Ub such that their do not exist
-    % singular values with multiplicity larger than one.
+sum_bad_idx_Ucc = sum(bad_idx_Ucc);
 
-    Ub=Ucc(:,bad_idx_Ucc);
+if sum_bad_idx_Ucc > 0.1
+    has_bad_part = 1;
 
-    S=rand(1,size(Ub,2))+1;
+    if sum_bad_idx_Ucc > s
+        priH = priH.*(rand(1,s)+1);
+        Ugq = csvdQ(priH);
+    else
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        % generating Ub
+        % the idea is to use every column of  Ucc(:,badIndex) to generated a
+        % quaternion column, and put them together, termed as Ub; Ub has 2t columns, while its column dim=t;
+        % thus then apply
+        % quaternion svd to Ub to obtain the true range. Of course, before applying
+        % svd, randomly scale every column of Ub such that their do not exist
+        % singular values with multiplicity larger than one.
+
+        Ub=Ucc(:,bad_idx_Ucc);
+
+        S=rand(1,size(Ub,2))+1;
 
 
-    % base = linspace(1, 10, size(Ub,2));  % 生成n个等间距的数值
-    % perturbation = 0.5 * rand(1, size(Ub,2)) - 0.25;  % 生成随机扰动，范围约为-0.25到+0.25
-    % S = abs(base + perturbation);  % 将扰动添加到基线数值上
+        % base = linspace(1, 10, size(Ub,2));  % 生成n个等间距的数值
+        % perturbation = 0.5 * rand(1, size(Ub,2)) - 0.25;  % 生成随机扰动，范围约为-0.25到+0.25
+        % S = abs(base + perturbation);  % 将扰动添加到基线数值上
 
-    Ub=Ub.*S;
+        Ub=Ub.*S;
 
-    [Uq,~,~]=csvdQ(Qc2Q(Ub));
+        [Uq,~,~]=csvdQ(Qc2Q(Ub));
 
-    Uq = Uq(:,1:size(Uq,2)/2);
+        Uq = Uq(:,1:size(Uq,2)/2);
 
-    % end of generating Ub
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        % end of generating Ub
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    end
 end
 
 
