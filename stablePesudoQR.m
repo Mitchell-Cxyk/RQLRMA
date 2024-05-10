6a4643d45ede273fe5d8337deab7de438cba36d9
yuningyang19 <51691872+yuningyang19@users.noreply.github.com>
2024-05-10 15:27:15 +0800

    new

---
+++ stablePesudoQR.m
@@ -0,0 +1,17 @@
+function H=stablePesudoQR(A)
+% first a qr step, followed by solving H'*HX = H' three times as the
+% correction steps
+H=pesudoQR(A);
+for iter=1:3
+    %disp('stablize');
+Z=QLEQL(H,H'*H);
+Pro=randq(size(Z,2),1);
+for iterPower=1:2
+Pro=Pro/norm(Pro);
+Pro=Z*Pro;
+Pro=Z'*Pro;
+epsilon=sqrt(norm(Pro));
+end
+H=H+Z/epsilon;
+end
+end
